cd67104d377db0115cb877347b07b094324d8c99
Nikolay Shevyrev <nikolay.shevyrev@gmail.com>
2021-07-19 14:50:43 +0300

    Initial commit

---
+++ Core/inc/main.h
@@ -0,0 +1,19 @@
+/*
+ * main.h
+ *
+ *  Created on: Oct 6, 2020
+ *      Author: Nikolay Shevyrev
+ */
+
+#ifndef CORE_INC_MAIN_H_
+#define CORE_INC_MAIN_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f722xx.h"
+#include "stm32f7xx.h"
+
+/* Function prototypes -------------------------------------------------------*/
+void SystemClock_Config(void);
+
+
+#endif /* CORE_INC_MAIN_H_ */
---
+++ Core/src/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 6, 2020
+ *      Author: Nikolay Shevyrev
+ */
+
+
+#include "main.h"
+
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(){
+
+	SystemClock_Config();
+
+	/* Infinite loop */
+	while(1);
+
+}
+
+
+
